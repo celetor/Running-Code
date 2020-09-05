7977ee8e41f42fbe0f8788ec3842f484659c8525
Knocknock <alansky114@gmail.com>
2020-09-05 13:49:43 +0800

    up

--- Python/wechat_robot.py
+++ Python/wechat_robot.py
@@ -181,7 +181,7 @@ def text_content():
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
     }
     ret = request(url, headers=headers).text
-    return ret.get('hitokoto')
+    return json.loads(ret).get('hitokoto')
 
 
 if __name__ == '__main__':

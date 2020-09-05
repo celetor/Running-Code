46b4cbd7341ff44ab813049bfecaed2cca4d8f9a
Knocknock <alansky114@gmail.com>
2020-09-05 13:44:05 +0800

    up

--- Python/wechat_robot.py
+++ Python/wechat_robot.py
@@ -175,10 +175,28 @@ def markdown_content():
     return request(url).text
 
 
+def text_content():
+    url = 'https://v1.hitokoto.cn/?encode=json'
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+    }
+    ret = request(url, headers=headers).text
+    return ret.get('hitokoto')
+
+
 if __name__ == '__main__':
     json_data = ''
+    if os.environ.get('IS_TEXT') == 'TRUE':
+        article = text_content()
+        print(article)
+        json_data = json.dumps({
+            "msgtype": "text",
+            "markdown": {
+                "content": article
+        }
+    })
     # 图片类型
-    if os.environ.get('IS_PICTURE') == 'TRUE':
+    elif os.environ.get('IS_PICTURE') == 'TRUE':
         article = picture_json()
         print(article)
         json_data = json.dumps({
